fdfd60f7ebb14db7e7fd827990e7aaf0080c1367
unknown <thaominh030617@gmail.com>
2021-09-01 00:27:12 +0700

    test

--- src/pages/Root/RootPage.tsx
+++ src/pages/Root/RootPage.tsx
@@ -4,6 +4,7 @@ const RootPage = () => {
     return (
         <>
             <h1>React Redux Boilerplate</h1>
+
             <p> hello Tibb, i can't become to a person who you want.</p>
         </>
     );
