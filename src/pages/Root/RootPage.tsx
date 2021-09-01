6004ca4b192a48058ba66721fb032232cdf903da
thiendangit <thiendangit1102@gmail.com>
2021-09-02 01:45:27 +0700

    edit husky

--- src/pages/Root/RootPage.tsx
+++ src/pages/Root/RootPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const RootPage = () => {
+const RootPage: React.FC = () => {
     return (
         <>
             <h1>React Redux Boilerplate</h1>
-
-            <p> hello Tibb, i can't become to a person who you want.</p>
+            <p> hello Tibb, i cant become to a person who you want.</p>
         </>
     );
 };
