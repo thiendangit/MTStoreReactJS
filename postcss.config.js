9bdfbec5f85dd73cb95efb6e3a93f660942ddb0e
thiendangit <thiendangit1102@gmail.com>
2021-09-02 02:20:13 +0700

    edit husky

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
-const tailwindcss = require('tailwindcss');
 module.exports = {
-    plugins: ['postcss-preset-env', tailwindcss],
+    plugins: [
+        'postcss-preset-env',
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('tailwindcss/nesting'),
+    ],
 };
