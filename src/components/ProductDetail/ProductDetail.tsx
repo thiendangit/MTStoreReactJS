adf86d132e4fb7cd6cfcf9c4193c0dd498e34097
unknown <thaominh030617@gmail.com>
2021-10-24 01:00:35 +0700

    change sowme css and detail

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -79,7 +79,9 @@ export const ProductDetail = () => {
         <div>
           <div className="flex flex-row justify-start items-center relative">
             <p className="product__detail-percent-btn ml-5">{item?.sale_price}</p>
-            <p className="product__detail-percent-btn ml-28">Free shipping</p>
+            <p className={item?.shipping_class !== '' ? 'product__detail-percent-btn ml-28' : 'hidden'}>
+              {item?.shipping_class !== '' ? item?.shipping_class : null}
+            </p>
           </div>
           <img
             src={item?.images?.[0]?.src ? item?.images?.[0]?.src : `${images.noImg}`}
