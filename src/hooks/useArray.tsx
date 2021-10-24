75d074ad9b9943ed0072e33a3eec0463ab7de4f4
thiendangit <thiendangit1102@gmail.com>
2021-10-24 20:53:54 +0700

    addOne useArray

---
+++ src/hooks/useArray.tsx
@@ -0,0 +1,138 @@
+import { useCallback, useState, useMemo, SetStateAction, useEffect } from 'react';
+
+export type EntityId = number | string;
+
+export interface DictionaryNum<T> {
+  [id: number]: T | undefined;
+}
+export interface Dictionary<T> extends DictionaryNum<T> {
+  [id: string]: T | undefined;
+}
+
+export type InitialArray<T extends any> = {
+  id: EntityId;
+} & T;
+
+export interface EntityState<T> {
+  ids: EntityId[];
+  entities: Dictionary<T>;
+}
+
+type UseStateFull<T = any> = {
+  value: T;
+  setValue: React.Dispatch<SetStateAction<T>>;
+};
+
+type UseArrayActions<T> = {
+  setValue: UseStateFull<T[]>['setValue'];
+  addOne: (entity: T) => void;
+  addMany?: (entities: T[]) => void;
+  setAll?: (entities: T[]) => void;
+  removeOne?: (entity: T) => void;
+  removeMany?: (entitys: T[]) => void;
+  removeAll?: () => void;
+  updateOne?: (entity: T) => void;
+  updateMany?: (entities: T[]) => void;
+  upsertOne?: (entity: T) => void;
+  upsertMany?: (entities: T[]) => void;
+};
+
+type UseArray<T = any> = [T[], UseArrayActions<T>];
+
+function useArray<T = any>(initial: T[]): UseArray<T> {
+  const [value, setValue] = useState<EntityState<T>>();
+
+  useEffect(() => {
+    // const initialTest = Array(10000).fill(0)
+    // const test1 = () => {
+    //     return initialTest.slice(0)
+    // }
+    // const test2 = () => {
+    //     return Object.values(initialTest)
+    // }
+    // const test3 = () => {
+    //     return Object.assign([], initialTest)
+    // }
+
+    // const test4 = () => {
+    //     return [...initialTest]
+    // }
+
+    // const test5 = () => {
+    //     return JSON.parse(JSON.stringify(initialTest))
+    // }
+
+    // const test6 = () => {
+    //     return Object.assign({}, initialTest)
+    // }
+
+    // const minRes = TestPerformance([test1, test2, test3, test4, test5, test6])
+
+    // console.log(minRes.minIdx);
+
+    if (!value && initial) {
+      setValue(
+        initial.reduce(
+          (old: EntityState<T>, current: InitialArray<T>) => {
+            const ids_copy = Object.values(old.ids);
+            ids_copy.push(current?.id);
+            return {
+              ids: ids_copy,
+              entities: { ...old.entities, [current?.id]: current },
+            };
+          },
+          {
+            ids: [],
+            entities: {},
+          },
+        ),
+      );
+    }
+  }, [initial]);
+
+  const addOne = useCallback((entity: InitialArray<T>) => {
+    setValue((prev: EntityState<T>) => {
+      if (!prev.entities[entity?.id]) {
+        prev.ids = [...prev.ids, entity?.id];
+        prev.entities[entity?.id] = entity;
+      }
+
+      return prev;
+    });
+  }, []);
+
+  const actions = useMemo(
+    () => ({
+      setValue,
+      addOne,
+    }),
+    [addOne],
+  );
+  return [value, actions];
+}
+
+// return idx of min
+// const TestPerformance = (arr: any[]): {min: number,minIdx: number} => {
+//     const arrResult = Array(arr.length);
+//     arr.map((el, idx) => {
+//         const timeStart = performance.now();
+//         const t = el();
+//         console.log({ t });
+//         arrResult[idx] = performance.now() - timeStart;
+//     })
+//     let min = arrResult[0];
+//     let minIdx = 0;
+//     console.log(arrResult)
+//     arrResult.map((el, idx) => {
+//         if (el < min) {
+//             min = el
+//             minIdx = idx
+//         }
+//     })
+//     return {
+//         min,
+//         minIdx
+//     }
+// }
+
+export { useArray };
--- src/pages/Root/RootPage.tsx
+++ src/pages/Root/RootPage.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import { Navbar, Header, MenuBar, Footer } from '@components';
-import { CallAPI } from '../../apis/initData';
-import { Container } from '@material-ui/core';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { LinkPage } from '../LinkPage/LinkPage';
+import React, { useEffect } from 'react';
+// import { Navbar, Header, MenuBar, Footer } from '@components';
+// import { CallAPI } from '../../apis/initData';
+// import { Container } from '@material-ui/core';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { LinkPage } from '../LinkPage/LinkPage';
+import { useArray } from '@hooks/useArray';
 
 const RootPage: React.FC = () => {
+  const [value, actions] = useArray([
+    { id: 222, name: 'tét' },
+    { id: 232, name: 'hihi' },
+    { id: 221, name: '@@@' },
+  ]);
+  console.log({ value });
+
+  useEffect(() => {
+    // actions
+    actions.addOne({ id: 1231, name: 'tét' });
+  }, []);
+
   return (
-    <Router>
-      <Container>
-        <Navbar />
-        <Header />
-        <MenuBar />
-        <LinkPage />
-        <Footer />
-        <CallAPI />
-      </Container>
-    </Router>
+    <div />
+    // <Router>
+    //   <Container>
+    //     <Navbar />
+    //     <Header />
+    //     <MenuBar />
+    //     <LinkPage />
+    //     <Footer />
+    //     <CallAPI />
+    //   </Container>
+    // </Router>
   );
 };
 
