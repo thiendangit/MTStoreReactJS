1259bd31911fe0c9879526dc42c89ec1cf48867c
unknown <thaominh030617@gmail.com>
2021-11-04 12:27:38 +0700

    add style in check out page

--- src/components/Checkout/components/PaymentMethod.tsx
+++ src/components/Checkout/components/PaymentMethod.tsx
@@ -7,8 +7,8 @@ export const PaymentMethod = () => {
     <div>
       <div>
         <h3>Billing method</h3>
-        <p className="text__color-gray">Please enter your payment method</p>
-        <div className="grid grid-cols-3 items-center w-full">
+        <p className="text__color-gray mb-6">Please enter your payment method</p>
+        <div className="grid grid-cols-3 items-center w-full bg-gray-200 rounded-xl px-3 mb-6">
           <div className="flex flex-row gap-2 items-center text__p">
             <Radio />
             <p>FedEx</p>
@@ -19,7 +19,7 @@ export const PaymentMethod = () => {
           </span>
           <img src={icons.fexex} alt={'FexEx'} className="w-16 ml-auto" />
         </div>
-        <div className="grid grid-cols-3 items-center w-full ">
+        <div className="grid grid-cols-3 items-center w-full bg-gray-200 rounded-xl px-3 mb-6">
           <div className="flex flex-row gap-2 items-center text__p">
             <Radio />
             <p>DHL</p>
@@ -33,15 +33,15 @@ export const PaymentMethod = () => {
       </div>
       <div>
         <h3>Payment method</h3>
-        <p className="text__color-gray">Please enter your payment method</p>
-        <div className="flex flex-row justify-between items-center w-full">
+        <p className="text__color-gray mb-6">Please enter your payment method</p>
+        <div className="flex flex-row justify-between items-center w-full bg-gray-200 rounded-xl px-3 mb-6">
           <div className="flex flex-row gap-2 items-center text__p">
             <Radio />
             <p>PayPal</p>
           </div>
           <img src={icons.paypal} alt={'PayPal'} className="w-20" />
         </div>
-        <div className="flex flex-row gap-2 items-center text__p">
+        <div className="flex flex-row gap-2 items-center text__p bg-gray-200 rounded-xl px-3 mb-6">
           <Radio />
           <p>Cash</p>
         </div>
