2b9c43b6bac8f680b53f9b11b75d0fb5fe5e1c0e
unknown <thaominh030617@gmail.com>
2021-11-08 03:30:10 +0700

    add yup in info customer of checkout page

--- src/pages/CheckoutPage/components/InfoCustomer.tsx
+++ src/pages/CheckoutPage/components/InfoCustomer.tsx
@@ -5,11 +5,25 @@ import { TextField } from '@material-ui/core';
 
 const validationSchema = yup.object({
   email: yup.string().email('Enter a valid email').required('Email is required'),
+  first_name: yup.string().min(2, 'Too short!').max(50, 'Too long!').required('First name is required'),
+  last_name: yup.string().min(2, 'Too short!').max(50, 'Too long!').required('Last name is required'),
+  address: yup.string().required('Address is required'),
+  phone: yup.string().required('Phone number is required'),
+  city: yup.string().required('Town/ City is required'),
+  country: yup.string().required('Country is required'),
+  code: yup.string().required('ZIP/ Postal code  is required'),
 });
 export const InfoCustomer: React.FC = () => {
   const formik = useFormik({
     initialValues: {
       email: '',
+      first_name: '',
+      last_name: '',
+      address: '',
+      phone: '',
+      city: '',
+      country: '',
+      code: '',
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -26,12 +40,20 @@ export const InfoCustomer: React.FC = () => {
           label="First name:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.first_name}
+          onChange={formik.handleChange}
+          error={formik.touched.first_name && Boolean(formik.errors.first_name)}
+          helperText={formik.touched.first_name && formik.errors.first_name}
         />
         <TextField
           name="last_name"
           label="Last name:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.last_name}
+          onChange={formik.handleChange}
+          error={formik.touched.last_name && Boolean(formik.errors.last_name)}
+          helperText={formik.touched.last_name && formik.errors.last_name}
         />
         <TextField
           id="email"
@@ -49,30 +71,50 @@ export const InfoCustomer: React.FC = () => {
           label="Phone number:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.phone}
+          onChange={formik.handleChange}
+          error={formik.touched.phone && Boolean(formik.errors.phone)}
+          helperText={formik.touched.phone && formik.errors.phone}
         />
         <TextField
           name="address"
           label="Address:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.address}
+          onChange={formik.handleChange}
+          error={formik.touched.address && Boolean(formik.errors.address)}
+          helperText={formik.touched.address && formik.errors.address}
         />
         <TextField
           name="city"
           label="Town / City:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.city}
+          onChange={formik.handleChange}
+          error={formik.touched.city && Boolean(formik.errors.city)}
+          helperText={formik.touched.city && formik.errors.city}
         />
         <TextField
           name="country"
           label="State / Country:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.country}
+          onChange={formik.handleChange}
+          error={formik.touched.country && Boolean(formik.errors.country)}
+          helperText={formik.touched.country && formik.errors.country}
         />
         <TextField
           name="code"
           label="ZIP/Postal code:"
           className="form_item input-styled rounded-3xl"
           variant={'outlined'}
+          value={formik.values.code}
+          onChange={formik.handleChange}
+          error={formik.touched.code && Boolean(formik.errors.code)}
+          helperText={formik.touched.code && formik.errors.code}
         />
       </form>
     </div>
